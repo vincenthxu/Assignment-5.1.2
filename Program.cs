22e025d3cf296c7f0dc1d0e34bbb40b43f9ed77e
Vincent Xu <vincent.xu1@outlook.com>
2025-08-04 12:56:23 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+namespace Assignment_5._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                foreach (var arg in args)
+                {
+                    if (int.TryParse(arg, out var value))
+                    {
+                        Console.WriteLine($"The sum of the digits of the number {value} is : {SumDigits(value)}");
+                    }
+                }
+            }
+        }
+
+        static int SumDigits(int num)
+        {
+            int sum = 0;
+            while(num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+    }
+}
